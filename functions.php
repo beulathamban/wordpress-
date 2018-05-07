6e08c4efb73842060840e7a03dad3d48a8a570cb
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-05-08 11:23:25 +1200

    functions.php

--- functions.php
+++ functions.php
@@ -84,6 +84,99 @@ function sample_remove_version() {
 add_filter('the_generator', 'sample_remove_version');
 
 
+/*
+
+==========================================
+	 Custom Post Type
+=========================================
+*/
+function sample_custom_post_type (){
+	
+	$labels = array(
+		'name' => 'Portfolio',
+		'singular_name' => 'Portfolio',
+		'add_new' => 'Add Item',
+		'all_items' => 'All Items',
+		'add_new_item' => 'Add Item',
+		'edit_item' => 'Edit Item',
+		'new_item' => 'New Item',
+		'view_item' => 'View Item',
+		'search_item' => 'Search Portfolio',
+		'not_found' => 'No items found',
+		'not_found_in_trash' => 'No items found in trash',
+		'parent_item_colon' => 'Parent Item'
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail',
+			'revisions',
+		),
+		//'taxonomies' => array('category', 'post_tag'),
+		'menu_position' => 5,
+		'exclude_from_search' => false
+	);
+	register_post_type('portfolio',$args);
+}
+add_action('init','sample_custom_post_type');
+
+//taxonomy
+
+function sample_custom_taxonomies(){
+	//add new taaxonomy hierarchical
+	$labels = array(
+		'name' => 'Types',
+		'singular_name' => 'Type',
+		'search_items' => 'Search Types',
+		'all_items' => 'All Types',
+		'parent_item' => 'Parent Type',
+		'parent_item_colon' => 'Parent Type:',
+		'edit_item' => 'Edit Type',
+		'update_item' => 'Update Type',
+		'add_new_item' => 'Add New Type',
+		'new_item_name' => 'New Type Name',
+		'menu_name' => 'Types',
+	);
+	$args=array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => array('slug'=> 'type'),
+
+	);
+
+register_taxonomy('type', array('portfolio'),$args);
+
+
+
+	//add new taaxonomy not hierarchical
+
+}
+add_action('init','sample_custom_taxonomies');
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
